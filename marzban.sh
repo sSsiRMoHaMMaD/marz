31226098f672338885111712ab4963fe8b1cbd48
sSsiRMoHaMMaD <130269418+sSsiRMoHaMMaD@users.noreply.github.com>
2023-12-05 14:20:25 +0100

    Update marzban.sh

--- marzban.sh
+++ marzban.sh
@@ -2,6 +2,8 @@
 
 # Get server name from the user
 read -p "Enter the server name: " SERVER
+# Get license key from the user
+read -p "Enter the license key: " LICENSE
 
 # Update the root password and set DNS
 echo 'root:sOn3lQ#bS@ls!7&m' | sudo chpasswd && \
@@ -74,14 +76,12 @@ echo 'root:sOn3lQ#bS@ls!7&m' | sudo chpasswd && \
   docker compose up -d && \
   cd && \
 
-# Get license key from the user
-read -p "Enter the license key: " LICENSE
 
 # Install WireGuard and configure Warp
 wget https://github.com/ViRb3/wgcf/releases/download/v2.2.19/wgcf_2.2.19_linux_amd64 && \
   mv wgcf_2.2.19_linux_amd64 /usr/bin/wgcf && \
   chmod +x /usr/bin/wgcf && \
-  wgcf register && \
+  echo -e "wgcf register\n" | wgcf && \
   wgcf generate && \
   sed -i '3s/.*/license_key = '$LICENSE'/' wgcf-account.toml && \
   wgcf update && \
