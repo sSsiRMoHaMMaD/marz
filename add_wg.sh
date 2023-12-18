7056233625f35c52f06ab391196443cd07d6de89
sSsiRMoHaMMaD <130269418+sSsiRMoHaMMaD@users.noreply.github.com>
2023-12-18 12:35:56 +0100

    Update add_wg.sh

--- add_wg.sh
+++ add_wg.sh
@@ -11,4 +11,4 @@ PersistentKeepalive = 25" >> /etc/wireguard/wg0.conf && \
 sed -i "s/\$PUBKEY/$PUBKEY/g" /etc/wireguard/wg0.conf && \
 sed -i "s/\$LOCAL_IP/$LOCAL_IP/g" /etc/wireguard/wg0.conf && \
 
-sudo wg-quick down wg0; sudo wg-quick up wg0
+sudo wg-quick down wg0
