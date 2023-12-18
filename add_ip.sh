d3885c94051d678d02660ea6fa91dd8528a6f8cf
sSsiRMoHaMMaD <130269418+sSsiRMoHaMMaD@users.noreply.github.com>
2023-12-18 13:37:01 +0100

    Update add_ip.sh

--- add_ip.sh
+++ add_ip.sh
@@ -7,4 +7,7 @@ sed -i '/#END_FILTER/i\-A FORWARD -d $LOCAL_IP\/32 -p tcp -m multiport --dports 
 
 sed -i '/#END_NAT/i\-A PREROUTING -p tcp -m multiport --dports $PORTS -j DNAT --to-destination $LOCAL_IP\n-A PREROUTING -p udp -m multiport --dports $PORTS -j DNAT --to-destination $LOCAL_IP\n-A POSTROUTING -d $LOCAL_IP/32 -p tcp -m multiport --dports $PORTS -j MASQUERADE\n-A POSTROUTING -d $LOCAL_IP/32 -p udp -m multiport --dports $PORTS -j MASQUERADE' /etc/iptables/rules.v4
 
+sed -i "s#$PORTS#$PORTS#g" /etc/iptables/rules.v4
+sed -i "s#$LOCAL_IP#$LOCAL_IP#g" /etc/iptables/rules.v4
+
 systemctl restart iptables
