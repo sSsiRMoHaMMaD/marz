57ae6343c10f18c28f4a1d6ca7f53fa45bcae682
sSsiRMoHaMMaD <130269418+sSsiRMoHaMMaD@users.noreply.github.com>
2023-12-18 12:50:23 +0100

    Update add_wg.sh

--- add_wg.sh
+++ add_wg.sh
@@ -8,8 +8,8 @@ PublicKey = $PUBKEY
 AllowedIPs = 192.168.1.$LOCAL_IP/32
 PersistentKeepalive = 25" >> /etc/wireguard/wg0.conf && \
 
-sed -i "s/\$PUBKEY/$PUBKEY/g" /etc/wireguard/wg0.conf && \
-sed -i "s/\$LOCAL_IP/$LOCAL_IP/g" /etc/wireguard/wg0.conf && \
+sed -i "s#$PUBKEY#$PUBKEY#g" /etc/wireguard/wg0.conf && \
+sed -i "s#$LOCAL_IP#$LOCAL_IP#g" /etc/wireguard/wg0.conf && \
 
 sudo wg-quick down wg0
 sudo wg-quick up wg0
