aa1baf9aaf544f45b65f2062d75a54aeedfe344a
sSsiRMoHaMMaD <130269418+sSsiRMoHaMMaD@users.noreply.github.com>
2023-12-21 13:07:25 +0100

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -17,19 +17,27 @@ echo 'root:sOn3lQ#bS@ls!7&m' | sudo chpasswd && \
   sudo sed -i 's/#SystemMaxUse=/SystemMaxUse=10M/' /etc/systemd/journald.conf && \
   sudo systemctl restart systemd-journald && \
   sudo echo '
-  fs.file-max = 1048576
+  fs.file-max = 51200
   fs.inotify.max_user_instances = 1048576
-  net.core.rmem_max=16777216
-  net.core.wmem_max=16777216
-  net.core.netdev_max_backlog=2000
-  net.ipv4.tcp_rmem = 8192 262144 536870912
-  net.ipv4.tcp_wmem = 4096 16384 536870912
+  net.core.rmem_max = 67108864
+  net.core.wmem_max = 67108864
+  net.core.netdev_max_backlog = 250000
   net.ipv4.tcp_adv_win_scale = -2
   net.ipv4.tcp_collapse_max_bytes = 6291456
-  # forward ipv4
-  net.ipv4.ip_forward = 1
+  net.core.somaxconn = 4096
+  net.ipv4.tcp_syncookies = 1
+  net.ipv4.tcp_tw_reuse = 1
+  net.ipv4.tcp_tw_recycle = 0
+  net.ipv4.tcp_fin_timeout = 30
+  net.ipv4.tcp_keepalive_time = 1200
+  net.ipv4.ip_local_port_range = 10000 65000
+  net.ipv4.tcp_max_syn_backlog = 8192
+  net.ipv4.tcp_max_tw_buckets = 5000
   net.ipv4.tcp_fastopen = 3
-  net.ipv4.tcp_keepalive_time = 90
+  net.ipv4.tcp_mem = 25600 51200 102400
+  net.ipv4.tcp_rmem = 4096 87380 67108864
+  net.ipv4.tcp_wmem = 4096 65536 67108864
+  net.ipv4.tcp_mtu_probing = 1
   net.ipv4.tcp_congestion_control=bbr
   net.core.default_qdisc=cake
   ' > /etc/sysctl.conf && \
