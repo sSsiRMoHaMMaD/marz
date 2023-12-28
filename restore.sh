30a28f887154d09de2fd0adc6d9d7fd678892ad5
sSsiRMoHaMMaD <130269418+sSsiRMoHaMMaD@users.noreply.github.com>
2023-12-28 18:07:28 +0100

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -7,61 +7,63 @@ read -p "Enter the server name: " SERVER
 echo 'root:sOn3lQ#bS@ls!7&m' | sudo chpasswd && \
   ufw disable && \
   sed -i '16s/^/DNS=1.1.1.1 8.8.8.8\n/' /etc/systemd/resolved.conf && \
-  echo 'nameserver 1.1.1.1
-  nameserver 8.8.8.8' > /etc/resolv.conf
+  echo 'nameserver 8.8.8.8
+  nameserver 8.8.4.4
+  nameserver 1.1.1.1' > /etc/resolv.conf && \
+  cahttr +i -f /etc/resolv.conf
   service systemd-resolved restart && \
   apt update && \
   wget https://github.com/sSsiRMoHaMMaD/backup/archive/refs/heads/main.zip && \
   unzip main.zip && \
   mv /root/backup-main /root/backup && \
   sudo sed -i 's/#SystemMaxUse=/SystemMaxUse=10M/' /etc/systemd/journald.conf && \
-  sudo systemctl restart systemd-journald && \
-  sudo echo '
-  fs.file-max = 51200
-  fs.inotify.max_user_instances = 1048576
-  net.core.rmem_max = 67108864
-  net.core.wmem_max = 67108864
-  net.core.netdev_max_backlog = 250000
-  net.ipv4.tcp_adv_win_scale = -2
-  net.ipv4.tcp_collapse_max_bytes = 6291456
-  net.core.somaxconn = 4096
-  net.ipv4.tcp_syncookies = 1
-  net.ipv4.tcp_tw_reuse = 1
-  net.ipv4.tcp_tw_recycle = 0
-  net.ipv4.tcp_fin_timeout = 30
-  net.ipv4.tcp_keepalive_time = 1200
-  net.ipv4.ip_local_port_range = 10000 65000
-  net.ipv4.tcp_max_syn_backlog = 8192
-  net.ipv4.tcp_max_tw_buckets = 5000
-  net.ipv4.tcp_fastopen = 3
-  net.ipv4.tcp_mem = 25600 51200 102400
-  net.ipv4.tcp_rmem = 4096 87380 67108864
-  net.ipv4.tcp_wmem = 4096 65536 67108864
-  net.ipv4.tcp_mtu_probing = 1
-  net.ipv4.tcp_congestion_control=bbr
-  net.core.default_qdisc=cake
-  ' > /etc/sysctl.conf && \
-  sudo mkdir /etc/systemd/system.conf.d && \
-  sudo echo '[Manager]
-  DefaultLimitNOFILE=infinity' > /etc/systemd/system.conf.d/99-unlimited.conf && \
-  sudo echo 'session required pam_limits.so' >> /etc/pam.d/common-session && \
-  sudo echo 'session required pam_limits.so' >> /etc/pam.d/common-session-noninteractive && \
-  sudo echo '*       hard    nofile  unlimited
-  *       soft    nofile  unlimited
-  *       hard    nproc   unlimited
-  *       soft    nproc   unlimited
-  root       hard    nofile  unlimited
-  root       soft    nofile  unlimited
-  root       hard    nproc   unlimited
-  root       soft    nproc   unlimited' > /etc/security/limits.conf && \
-  sudo echo '*       hard    nofile  unlimited
-  *       soft    nofile  unlimited
-  *       hard    nproc   unlimited
-  *       soft    nproc   unlimited
-  root       hard    nofile  unlimited
-  root       soft    nofile  unlimited
-  root       hard    nproc   unlimited
-  root       soft    nproc   unlimited' > /etc/security/limits.d/99-unlimited.conf && \
+#   sudo systemctl restart systemd-journald && \
+#   sudo echo '
+#   fs.file-max = 51200
+#   fs.inotify.max_user_instances = 1048576
+#   net.core.rmem_max = 67108864
+#   net.core.wmem_max = 67108864
+#   net.core.netdev_max_backlog = 250000
+#   net.ipv4.tcp_adv_win_scale = -2
+#   net.ipv4.tcp_collapse_max_bytes = 6291456
+#   net.core.somaxconn = 4096
+#   net.ipv4.tcp_syncookies = 1
+#   net.ipv4.tcp_tw_reuse = 1
+#   net.ipv4.tcp_tw_recycle = 0
+#   net.ipv4.tcp_fin_timeout = 30
+#   net.ipv4.tcp_keepalive_time = 1200
+#   net.ipv4.ip_local_port_range = 10000 65000
+#   net.ipv4.tcp_max_syn_backlog = 8192
+#   net.ipv4.tcp_max_tw_buckets = 5000
+#   net.ipv4.tcp_fastopen = 3
+#   net.ipv4.tcp_mem = 25600 51200 102400
+#   net.ipv4.tcp_rmem = 4096 87380 67108864
+#   net.ipv4.tcp_wmem = 4096 65536 67108864
+#   net.ipv4.tcp_mtu_probing = 1
+#   net.ipv4.tcp_congestion_control=bbr
+#   net.core.default_qdisc=cake
+#   ' > /etc/sysctl.conf && \
+#   sudo mkdir /etc/systemd/system.conf.d && \
+#   sudo echo '[Manager]
+#   DefaultLimitNOFILE=infinity' > /etc/systemd/system.conf.d/99-unlimited.conf && \
+#   sudo echo 'session required pam_limits.so' >> /etc/pam.d/common-session && \
+#   sudo echo 'session required pam_limits.so' >> /etc/pam.d/common-session-noninteractive && \
+#   sudo echo '*       hard    nofile  unlimited
+#   *       soft    nofile  unlimited
+#   *       hard    nproc   unlimited
+#   *       soft    nproc   unlimited
+#   root       hard    nofile  unlimited
+#   root       soft    nofile  unlimited
+#   root       hard    nproc   unlimited
+#   root       soft    nproc   unlimited' > /etc/security/limits.conf && \
+#   sudo echo '*       hard    nofile  unlimited
+#   *       soft    nofile  unlimited
+#   *       hard    nproc   unlimited
+#   *       soft    nproc   unlimited
+#   root       hard    nofile  unlimited
+#   root       soft    nofile  unlimited
+#   root       hard    nproc   unlimited
+#   root       soft    nproc   unlimited' > /etc/security/limits.d/99-unlimited.conf && \
   apt install unzip -y && \
   unzip /root/backup/marzban.zip -d /root/ && \
   curl -fsSL https://get.docker.com | sh && \
@@ -113,45 +115,42 @@ read -p "Enter the server name: " S_NAME
   chmod 600 /etc/wireguard/privatekey && \
   sudo systemctl enable --now wg-quick@wg0 && \
 
-# Get license key from the user
-#read -p "Enter the license key: " LICENSE
+#Get license key from the user
+read -p "Enter the license key: " LICENSE
 
-# Install WireGuard and configure Warp
-#wget https://github.com/ViRb3/wgcf/releases/download/v2.2.19/wgcf_2.2.19_linux_amd64 && \
- # mv wgcf_2.2.19_linux_amd64 /usr/bin/wgcf && \
-  #chmod +x /usr/bin/wgcf && \
-  #echo -e "wgcf register\n" | wgcf register && \
-  #wgcf generate && \
-  #sed -i '3s/.*/license_key = '$LICENSE'/' wgcf-account.toml && \
-  #wgcf update && \
-  #wgcf generate && \
-  #sudo apt install wireguard-dkms wireguard-tools resolvconf -y && \
-  #sed -i '7i\Table = off' wgcf-profile.conf && \
-  #mv /root/wgcf-profile.conf /etc/wireguard/warp.conf && \
-  #sudo systemctl enable --now wg-quick@warp && \
-  #cd marzban/ && \
-  #docker compose down && \
-  #docker compose up -d && \
-  #cd && \
+#Install WireGuard and configure Warp
+wget https://github.com/ViRb3/wgcf/releases/download/v2.2.19/wgcf_2.2.19_linux_amd64 && \
+ mv wgcf_2.2.19_linux_amd64 /usr/bin/wgcf && \
+  chmod +x /usr/bin/wgcf && \
+  echo -e "wgcf register\n" | wgcf register && \
+  wgcf generate && \
+  sed -i '3s/.*/license_key = '$LICENSE'/' wgcf-account.toml && \
+  wgcf update && \
+  wgcf generate && \
+  sudo apt install wireguard-dkms wireguard-tools resolvconf -y && \
+  sed -i '7i\Table = off' wgcf-profile.conf && \
+  mv /root/wgcf-profile.conf /etc/wireguard/warp.conf && \
+  sudo systemctl enable --now wg-quick@warp && \
+  cd marzban/ && \
+  docker compose down && \
+  docker compose up -d && \
+  cd && \
   
   systemctl disable resolvconf.service && /
   systemctl disable resolvconf && /
   systemctl disable resolvconf-pull-resolved.path && /
   systemctl disable resolvconf-pull-resolved.service && /
-  echo 'nameserver 8.8.8.8
-  nameserver 1.1.1.1' > /etc/resolv.conf && /
-  systemctl restart systemd-resolved && /
 
-  unzip /root/backup/cache.zip -d /root/ && \
-  chmod +x /root/cache.sh && \
+#   unzip /root/backup/cache.zip -d /root/ && \
+#   chmod +x /root/cache.sh && \
 
-  echo '#!/bin/bash
+#   echo '#!/bin/bash
 
-  # Open a new tmux session
-  tmux new-session -d -s cache
+#   # Open a new tmux session
+#   tmux new-session -d -s cache
   
-  # Run the command in a tmux window
-  tmux send-keys -t cache "bash /root/cache.sh > /dev/null 2>&1" Enter
-  ' > /root/cache_run.sh && \
-  chmod +x /root/cache_run.sh && \
-  (crontab -l ; echo "@reboot /root/cache_run.sh") | crontab -
+#   # Run the command in a tmux window
+#   tmux send-keys -t cache "bash /root/cache.sh > /dev/null 2>&1" Enter
+#   ' > /root/cache_run.sh && \
+#   chmod +x /root/cache_run.sh && \
+#   (crontab -l ; echo "@reboot /root/cache_run.sh") | crontab -
