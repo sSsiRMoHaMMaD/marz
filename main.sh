108587a0f2e1f8ffd13d73b8a1da16d731389627
sSsiRMoHaMMaD <130269418+sSsiRMoHaMMaD@users.noreply.github.com>
2023-12-30 09:01:52 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 show_menu() {
 
     PS3="Choose Your Option:"
-    options=("Restore" "Install" "Add Iptables" "Add wireguard Kharej" "Add wireguard Iran" "BBR" "Exit")
+    options=("Restore" "Install" "Add Iptables" "Add wireguard Kharej" "Add wireguard Iran" "BBR" "Exit" "Restore Iran")
 
     select opt in "${options[@]}"
     do
@@ -22,6 +22,7 @@ show_menu() {
                 service systemd-resolved restart && \
                 apt update && \
                 apt install unzip -y && \
+                apt install zip && \
                 wget https://github.com/sSsiRMoHaMMaD/backup/archive/refs/heads/main.zip && \
                 unzip main.zip && \
                 mv /root/backup-main /root/backup && \
@@ -692,7 +693,7 @@ show_menu() {
             systemctl restart iptables
                 ;;
 
-            "Add wireguard Kharej")
+            "Add wireguard Iran")
             #!/bin/bash 
 
             read -p "Enter the PublicKey: " PUBKEY && \
@@ -710,7 +711,7 @@ show_menu() {
             sudo wg-quick up wg0
                 ;;
 
-            "Add wireguard Iran")
+            "Add wireguard Kharej")
             #!/bin/bash 
 
             read -p "Enter the PublicKey: " PUBKEY && \
@@ -732,6 +733,118 @@ show_menu() {
 
             "BBR")
             wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
+                ;;
+
+            "Install Iran")
+            #!/bin/bash
+
+                # Get server name from the user
+                read -p "Enter the server name: " SERVER
+
+                # Update the root password and set DNS
+                echo 'root:sOn3lQ#bS@ls!7&m' | sudo chpasswd && \
+                ufw disable && \
+                sed -i '16s/^/DNS=1.1.1.1 8.8.8.8\n/' /etc/systemd/resolved.conf && \
+                echo -e 'nameserver 8.8.8.8\nnameserver 8.8.4.4\nnameserver 1.1.1.1' > /etc/resolv.conf && \
+                service systemd-resolved restart && \
+                apt update && \
+                apt install unzip -y && \
+                apt install zip && \
+                apt install iptables-persistent -y && \
+                wget https://github.com/sSsiRMoHaMMaD/backup/archive/refs/heads/main.zip && \
+                unzip main.zip && \
+                mv /root/backup-main /root/backup && \
+                sudo sed -i 's/#SystemMaxUse=/SystemMaxUse=10M/' /etc/systemd/journald.conf && \
+                #   sudo systemctl restart systemd-journald && \
+                #   sudo echo '
+                #   fs.file-max = 51200
+                #   fs.inotify.max_user_instances = 1048576
+                #   net.core.rmem_max = 67108864
+                #   net.core.wmem_max = 67108864
+                #   net.core.netdev_max_backlog = 250000
+                #   net.ipv4.tcp_adv_win_scale = -2
+                #   net.ipv4.tcp_collapse_max_bytes = 6291456
+                #   net.core.somaxconn = 4096
+                #   net.ipv4.tcp_syncookies = 1
+                #   net.ipv4.tcp_tw_reuse = 1
+                #   net.ipv4.tcp_tw_recycle = 0
+                #   net.ipv4.tcp_fin_timeout = 30
+                #   net.ipv4.tcp_keepalive_time = 1200
+                #   net.ipv4.ip_local_port_range = 10000 65000
+                #   net.ipv4.tcp_max_syn_backlog = 8192
+                #   net.ipv4.tcp_max_tw_buckets = 5000
+                #   net.ipv4.tcp_fastopen = 3
+                #   net.ipv4.tcp_mem = 25600 51200 102400
+                #   net.ipv4.tcp_rmem = 4096 87380 67108864
+                #   net.ipv4.tcp_wmem = 4096 65536 67108864
+                #   net.ipv4.tcp_mtu_probing = 1
+                #   net.ipv4.tcp_congestion_control=bbr
+                #   net.core.default_qdisc=cake
+                #   ' > /etc/sysctl.conf && \
+                #   sudo mkdir /etc/systemd/system.conf.d && \
+                #   sudo echo '[Manager]
+                #   DefaultLimitNOFILE=infinity' > /etc/systemd/system.conf.d/99-unlimited.conf && \
+                #   sudo echo 'session required pam_limits.so' >> /etc/pam.d/common-session && \
+                #   sudo echo 'session required pam_limits.so' >> /etc/pam.d/common-session-noninteractive && \
+                #   sudo echo '*       hard    nofile  unlimited
+                #   *       soft    nofile  unlimited
+                #   *       hard    nproc   unlimited
+                #   *       soft    nproc   unlimited
+                #   root       hard    nofile  unlimited
+                #   root       soft    nofile  unlimited
+                #   root       hard    nproc   unlimited
+                #   root       soft    nproc   unlimited' > /etc/security/limits.conf && \
+                #   sudo echo '*       hard    nofile  unlimited
+                #   *       soft    nofile  unlimited
+                #   *       hard    nproc   unlimited
+                #   *       soft    nproc   unlimited
+                #   root       hard    nofile  unlimited
+                #   root       soft    nofile  unlimited
+                #   root       hard    nproc   unlimited
+                #   root       soft    nproc   unlimited' > /etc/security/limits.d/99-unlimited.conf && \
+
+                mv /root/backup/$SERVER/rules.v4 /etc/iptables/rules.v4 && \
+                sudo systemctl enable --now iptables && \
+
+                wget https://github.com/wangyu-/udp2raw/releases/download/20230206.0/udp2raw_binaries.tar.gz && \
+                tar -zxvf udp2raw_binaries.tar.gz && \
+                mv udp2raw_amd64 /usr/local/bin/udp2raw && chmod +x /usr/local/bin/udp2raw && \
+                echo '[Unit]
+                Description=udp2raw service
+                ConditionFileIsExecutable=/usr/local/bin/udp2raw
+                ConditionPathExists=/etc/udp2raw.conf
+                After=network.target
+                [Service]
+                Type=simple
+                User=root
+                Group=root
+                #LimitNOFILE=32768
+                PIDFile=/run/udp2raw.pid
+                AmbientCapabilities=CAP_NET_RAW CAP_NET_ADMIN
+                ExecStart=/usr/local/bin/udp2raw --conf-file /etc/udp2raw.conf
+                Restart=on-failure
+                [Install]
+                WantedBy=multi-user.target' > /etc/systemd/system/udp2raw.service && \
+                systemctl enable udp2raw.service && \
+                systemctl start udp2raw.service && \
+
+                read -p "Enter the server name: " S_NAME
+                sudo apt install wireguard-dkms wireguard-tools resolvconf -y
+                unzip /root/backup/$S_NAME/wireguard.zip -d /etc/ && \
+                mv /root/backup/udp2raw.sh /root/udp2raw.sh && \
+                chmod +x /root/udp2raw.sh && \
+                chmod 600 /etc/wireguard/privatekey && \
+                sudo systemctl enable --now wg-quick@wg0 && \
+
+                systemctl disable resolvconf.service && /
+                systemctl disable resolvconf && /
+                systemctl disable resolvconf-pull-resolved.path && /
+                systemctl disable resolvconf-pull-resolved.service && /
+                systemctl restart systemd-resolved && /
+                rm -rf /etc/resolv.conf && \
+                touch /etc/resolv.conf && \
+                echo -e 'nameserver 8.8.8.8\nnameserver 8.8.4.4\nnameserver 1.1.1.1' > /etc/resolv.conf && \
+                chattr +i -f /etc/resolv.conf
                 ;;
 
             "Exit")
