9b3cedbc2ab43fb6f900e96c452488e574f7263d
sSsiRMoHaMMaD <130269418+sSsiRMoHaMMaD@users.noreply.github.com>
2024-07-02 11:48:47 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -130,23 +130,23 @@ show_menu() {
                 read -p "Enter the license key: " LICENSE
 
                 #Install WireGuard and configure Warp
-                wget https://github.com/ViRb3/wgcf/releases/download/v2.2.19/wgcf_2.2.19_linux_amd64 && \
-                mv wgcf_2.2.19_linux_amd64 /usr/bin/wgcf && \
-                chmod +x /usr/bin/wgcf && \
-                sleep 1 && \
-                echo -e "wgcf register\n" | wgcf register && \
-                sleep 1 && \
-                wgcf generate && \
-                sleep 1 && \
-                sed -i '3s/.*/license_key = '$LICENSE'/' wgcf-account.toml && \
-                sleep 1 && \
-                wgcf update && \
-                sleep 1 && \
-                wgcf generate && \
-                sudo apt install wireguard-dkms wireguard-tools resolvconf -y && \
-                sed -i '7i\Table = off' wgcf-profile.conf && \
-                mv /root/wgcf-profile.conf /etc/wireguard/warp.conf && \
-                sudo systemctl enable --now wg-quick@warp && \
+                #wget https://github.com/ViRb3/wgcf/releases/download/v2.2.19/wgcf_2.2.19_linux_amd64 && \
+                #mv wgcf_2.2.19_linux_amd64 /usr/bin/wgcf && \
+                #chmod +x /usr/bin/wgcf && \
+                #sleep 1 && \
+                #echo -e "wgcf register\n" | wgcf register && \
+                #sleep 1 && \
+                #wgcf generate && \
+                #sleep 1 && \
+                #sed -i '3s/.*/license_key = '$LICENSE'/' wgcf-account.toml && \
+                #sleep 1 && \
+                #wgcf update && \
+                #sleep 1 && \
+                #wgcf generate && \
+                #sudo apt install wireguard-dkms wireguard-tools resolvconf -y && \
+                #sed -i '7i\Table = off' wgcf-profile.conf && \
+                #mv /root/wgcf-profile.conf /etc/wireguard/warp.conf && \
+                #sudo systemctl enable --now wg-quick@warp && \
                 cd marzban/ && \
                 docker compose down && \
                 docker compose up -d && \
